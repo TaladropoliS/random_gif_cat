17f84806cf612d4163b8ff311e1be63450145af8
Fernando Taladriz <fernando.taladriz@gmail.com>
2022-06-26 16:22:27 -0400

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,135 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="p-sm-5">
+
+    <div class="card rounded-3">
+      <div class="card-title">
+        <h1 class="mt-3">Random Gift Cat</h1>
+      </div>
+      <div class="card-body">
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="titulo">Título</span>
+          <input v-model="titulo" type="text" class="form-control"
+                 name="titulo" placeholder="Título" aria-label="Título"
+                 aria-describedby="titulo" required>
+        </div>
+        <div class="row">
+          <div class="col-sm-6">
+            <select v-model="selectedFiltro" class="form-select mb-3" name="filtro" id="filtro"
+                    aria-label="Default select example" required>
+              <option v-for="(option, index) in optionsFiltro" :key="index" :value="option.value">
+                {{ option.text }}
+              </option>
+            </select>
+          </div>
+
+          <div class="col-sm-6">
+            <select v-model="selectedColor" class="form-select mb-3" name="color" id="color"
+                    aria-label="Default select example" required>
+              <option v-for="(option, index) in optionsColor" :key="index" :value="option.value">
+                {{ option.text }}
+              </option>
+            </select>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="tamanio">Tamaño</span>
+          <input v-model.number="tamanio" type="number" class="form-control" name="tamanio" placeholder="Tamaño"
+                 aria-label="Tamaño" required
+                 aria-describedby="tamanio">
+        </div>
+
+        <div class="p-2 mb-2 border rounded-3">
+          {{ titulo }} {{ selectedFiltro }} {{ selectedColor }} {{ tamanio }}
+        </div>
+
+        <button @click.prevent="llamarCat(titulo, filtro, color, tamanio)" type="submit"
+                class="btn btn-primary">
+          <i class="fas fa-search"></i>
+          Buscar Cat
+        </button>
+      </div>
+
+      <div class="card-img">
+        <img v-show="isLoaded" v-if="this.cat" :src="this.cat" @load="onImgLoad" alt="gato"
+             class="img-fluid">
+        <div v-show="!isLoaded" class="spinner-border text-secondary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {},
+    data() {
+      return {
+        cat: '',
+        titulo: '',
+        filtro: '',
+        color: '',
+        tamanio: '',
+        selectedColor: '',
+        optionsColor: [
+          {text: 'Naranjo', value: 'orange'},
+          {text: 'Blanco', value: 'white'},
+          {text: 'Rojo', value: 'red'},
+          {text: 'Verde', value: 'green'},
+          {text: 'Amarillo', value: 'yellow'},
+          {text: 'Azul', value: 'blue'},
+        ],
+        selectedFiltro: '',
+        optionsFiltro: [
+          {text: 'Blur', value: 'blur'},
+          {text: 'Mono', value: 'mono'},
+          {text: 'Sepia', value: 'sepia'},
+          {text: 'Negative', value: 'negative'},
+          {text: 'Paint', value: 'paint'},
+          {text: 'Pixel', value: 'pixel'},
+        ],
+        isLoaded: false,
+      }
+    },
+
+    methods: {
+      async llamarCat(ti, fi, co, ta) {
+        console.log(this.titulo)
+        console.log(this.selectedFiltro)
+        console.log(this.selectedColor)
+        console.log(this.tamanio)
+        if (this.titulo && this.selectedFiltro && this.selectedColor && this.tamanio) {
+          this.isLoaded = false
+          ti = this.titulo
+          fi = this.selectedFiltro
+          co = this.selectedColor
+          ta = this.tamanio
+          try {
+            this.cat = await (
+                `https://cataas.com/cat/gif/says/${ti}?filter=${fi}&color=${co}&size=${ta}&type=or`
+            )
+            console.log(`https://cataas.com/cat/gif/says/${ti}?filter=${fi}&color=${co}&size=${ta}&type=or`)
+          } catch (error) {
+            // console.log(error)
+          }
+        }
+      },
+      onImgLoad() {
+        this.isLoaded = true
+      }
+    },
+    created() {
+      this.titulo = 'holi'
+      this.selectedFiltro = 'sepia'
+      this.selectedColor = 'orange'
+      this.tamanio = '80'
+      this.llamarCat('holi', 'sepia', 'orange', '80')
+    }
   }
-}
 </script>
 
 <style>
@@ -23,6 +139,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
