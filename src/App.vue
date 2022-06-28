2ed48f3d000ec0247e7d9be5a94699fb0551b10d
Fernando Taladriz <fernando.taladriz@gmail.com>
2022-06-28 08:52:36 -0400

    color

--- src/App.vue
+++ src/App.vue
@@ -6,50 +6,59 @@
         <h1 class="mt-3">Random Gift Cat</h1>
       </div>
       <div class="card-body">
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="titulo">Título</span>
-          <input v-model="titulo" type="text" class="form-control"
-                 name="titulo" placeholder="Título" aria-label="Título"
-                 aria-describedby="titulo" required>
-        </div>
         <div class="row">
-          <div class="col-sm-6">
-            <select v-model="selectedFiltro" class="form-select mb-3" name="filtro" id="filtro"
-                    aria-label="Default select example" required>
-              <option v-for="(option, index) in optionsFiltro" :key="index" :value="option.value">
-                {{ option.text }}
-              </option>
-            </select>
+          <div class="col-lg-3">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="titulo">Título</span>
+              <input v-model="titulo" type="text" class="form-control"
+                     name="titulo" placeholder="Título" aria-label="Título"
+                     aria-describedby="titulo" required>
+            </div>
           </div>
-          <div class="col-sm-6">
-            <div class="row mb-3">
-              <div class="col-9">
-                <select v-model="selectedColor" class="form-select" name="color" id="color"
+          <div class="col-lg-6">
+            <div class="row">
+              <div class="col-sm-6">
+                <select v-model="selectedFiltro" class="form-select mb-3" name="filtro" id="filtro"
                         aria-label="Default select example" required>
-                  <option v-for="(option, index) in optionsColor" :key="index" :value="option.value">
+                  <option v-for="(option, index) in optionsFiltro" :key="index" :value="option.value">
                     {{ option.text }}
                   </option>
                 </select>
               </div>
-              <div class="col-3 text-center">
-                <div class="mx-auto my-auto" :style="backgroundColor"
-                     style="border: solid 1px black; border-radius: 50%; width: 30px; height: 30px;">
+              <div class="col-sm-6">
+                <div class="row mb-3">
+                  <div class="col-9">
+                    <select v-model="selectedColor" class="form-select" name="color" id="color"
+                            aria-label="Default select example" required>
+                      <option v-for="(option, index) in optionsColor" :key="index" :value="option.value">
+                        {{ option.text }}
+                      </option>
+                    </select>
+                  </div>
+                  <div class="col-3 text-center">
+                    <div class="mx-auto my-auto" :style="background"
+                         style="border: solid 1px black; border-radius: 50%; width: 30px; height: 30px;">
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <div class="col-lg-3">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="tamanio">Tamaño</span>
+              <input v-model.number="tamanio" type="number" class="form-control" name="tamanio" placeholder="Tamaño"
+                     aria-label="Tamaño" required
+                     aria-describedby="tamanio">
+            </div>
+          </div>
         </div>
-
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="tamanio">Tamaño</span>
-          <input v-model.number="tamanio" type="number" class="form-control" name="tamanio" placeholder="Tamaño"
-                 aria-label="Tamaño" required
-                 aria-describedby="tamanio">
-        </div>
-
         <div class="p-2 mb-2 border rounded-3">
-          <i class="fa-solid fa-cat fa-3x"></i> nombre: <b>{{ titulo }}</b>, filtro: <b>{{ selectedFiltro }}</b>,
-          color <b>{{ selectedColor }}</b>, tamaño: <b>{{ tamanio }}</b>
+          <i class="fa-solid fa-cat fa-3x"></i>
+          nombre: <b>{{ titulo }}</b>,
+          filtro: <b>{{ selectedFiltro }}</b>,
+          color <span class="fw-bold" :style="color"><b>{{ selectedColor }}</b></span>,
+          tamaño: <b>{{ tamanio }}</b>
         </div>
 
         <button @click.prevent="llamarCat(titulo, selectedFiltro, selectedColor, tamanio)"
@@ -84,12 +93,12 @@
         tamanio: '',
         selectedColor: '',
         optionsColor: [
-          {text: 'Naranjo', value: 'orange'},
-          {text: 'Negro', value: 'black'},
+          {text: 'Naranjo', value: 'darkorange'},
+          {text: 'Indigo', value: 'indigo'},
           {text: 'Rojo', value: 'red'},
           {text: 'Verde', value: 'green'},
-          {text: 'Amarillo', value: 'yellow'},
-          {text: 'Azul', value: 'blue'},
+          {text: 'Café', value: 'saddlebrown'},
+          {text: 'Azul', value: 'royalblue'},
         ],
         selectedFiltro: '',
         optionsFiltro: [
@@ -101,15 +110,26 @@
           {text: 'Pixel', value: 'pixel'},
         ],
         isLoaded: false,
+        ti:'',
+        fi:'',
+        co:'',
+        ta:'',
       }
     },
     computed: {
-      backgroundColor() {
+      background() {
         return {
           'background-color': this.selectedColor,
-          'color': this.selectedColor
         }
-      }
+      },
+      color() {
+        return {
+          'background-color': this.selectedColor,
+          'color': 'white',
+          'padding': '5px',
+          'border-radius': '15px',
+        }
+      },
     },
     methods: {
       async llamarCat(ti, fi, co, ta) {
@@ -133,10 +153,10 @@
       },
     },
     created() {
-      this.titulo = 'holi'
-      this.selectedFiltro = 'sepia'
-      this.selectedColor = 'orange'
-      this.tamanio = '80'
+      this.titulo = 'rex'
+      this.selectedFiltro = 'paint'
+      this.selectedColor = 'indigo'
+      this.tamanio = '100'
       this.llamarCat('holi', 'sepia', 'orange', '80')
     }
   }
